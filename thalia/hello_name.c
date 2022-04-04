fe352c4b848de3c06eed388b3971abc55b1bd4f6
Sam Watkins <sam@ucm.dev>
2022-04-04 21:34:56 +1000

    Thalia wrote three little programs
    - hello world
    - hello name
    - simple maths

---
+++ thalia/hello_name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char *name = "Bongo2";
+
+	printf("Hello %s\n", name);
+}
---
+++ thalia/maths.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 18;
+	int b = 6;
+
+	printf("Hello %d\n", a * b);
+}
