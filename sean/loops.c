6771358c99476df3b27d8a67645ce494814065f0
Sam Watkins <sam@ucm.dev>
2022-04-05 19:52:28 +1000

    Sean learned about for and while loops.

---
+++ sean/loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 100; i++) {
+		printf("Hello, Jerome. ");
+	}
+	
+	int i = 0;
+	
+	while (i < 1000) {
+		printf("%d ", i);
+		i = i + 1;
+	}
+
+	return 0;
+}
